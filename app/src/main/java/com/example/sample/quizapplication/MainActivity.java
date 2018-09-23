23e160b9141877204c52b85b481c72d63b4d919b
yuuki shinpo <yuuki.shinpo@gmail.com>
2018-09-23 12:04:12 +0900

    0923aaaaaaaaaaaaaaaa- Merge branch 'master' of https://github.com/yuuki-shinpo/QuizApplication00
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser

--- app/src/main/java/com/example/sample/quizapplication/MainActivity.java
+++ app/src/main/java/com/example/sample/quizapplication/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public  void nananna(){
-
+      //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
     }
 }
